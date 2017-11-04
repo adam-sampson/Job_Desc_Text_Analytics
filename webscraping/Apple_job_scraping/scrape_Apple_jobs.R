ea53b93f6ee60f5def3e95824ae18eaaec9ef3ff
Adam <12676475+adam-sampson@users.noreply.github.com>
2017-11-04 16:39:42 -0400

    Beta candidate for scrape_Apple_jobs.R

--- webscraping/Apple_job_scraping/scrape_Apple_jobs.R
+++ webscraping/Apple_job_scraping/scrape_Apple_jobs.R
@@ -2,8 +2,11 @@ library(RSelenium)
 library(XML)
 library(stringr)
 
-remDr <- remoteDriver(remoteServerAddr = "192.168.99.100",
-                      +                       port = 4445L)
+# Set the working directory where 
+# Project directory starts at something like: setwd("~/MSA/Fall 2017/Job_Desc_Text_Analytics")
+setwd(paste0(getwd(),"/webscraping/Apple_job_scraping/files/"))
+
+remDr <- remoteDriver(remoteServerAddr = "192.168.99.100",port = 4445L)
 remDr$open()
 
 ## Test connection Visually
@@ -23,40 +26,63 @@ remDr$open()
   remDr$navigate(searchURL)
   remDr$screenshot(display = TRUE) 
 
-  # Results are located under the following XPath
-  # //*[@id="resultsDiv"]
+saveJobsFromPage <- function() {
+  ## Get the number of elements in the jobs list on a page
+  ## On the Apple Jobs site the xpath to only find links to jobs is: "//*[@id=\"jobs_list\"]/tbody/tr/td/p/a"
+  jobListElem <- remDr$findElements('xpath',"//*[@id=\"jobs_list\"]/tbody/tr/td/p/a")
   
-  # Pagination (Next) is located under 
-  # //*[@id="resultsDiv"]/ul/li[2]/a[2] or under a name "nextLink"
-  # this next page link is only valid if class is "arrow_next enabled"
+  ## Cycle through each link on the page using a random delay between clicking links. Save results to a file
+  for(jobLink in jobListElem) {
+    jobLink$clickElement()
+    remDr$screenshot(display = TRUE)
+      jobDescElem <- remDr$findElement('xpath', "//div[@class=\"requisition\" and not(contains(@style,'display: none'))]//div[@id=\"job\"]")
+        jobDescRaw <- jobDescElem$getElementAttribute("outerHTML")[[1]]
+        jobDescText <- htmlParse(jobDescRaw, asText = TRUE, encoding="UTF-8")
+          jobID <- capture.output(xpathApply(jobDescText, "//li")[[1]])
+          jobID <- str_replace_all(jobID,"<\\/?li>","")
+          jobID <- str_replace_all(jobID,":","")
+          jobID <- str_trim(jobID)
+          jobID <- str_replace_all(jobID," ","_")
+        print(paste0("Writing job ",jobID))
+        write(paste0("Writting job desc: ",jobID),file="log.txt",append = TRUE)
+        saveXML(jobDescText,
+                file = paste0(jobID,".xml"))
+          # Get back with htmlParse("Job_Number_113110538.xml")
+    Sys.sleep(round(runif(n=1,min=5,max=15),digits = 3))
+  }
+} 
   
-  # List of results are located under //*[@id="jobs_list"]
-  # Each row is it's own element //*[@id="jobs_list"]/tbody/tr[1]
-  # Each row that has jobs data has the class "searchresult"
-  # The first column/td with class "title" and id format //*[@id="requisition-70818512"] contains the link
-  # The link is inside a <p> tag
-  # Each link to a job is in an <a> tag that has the XPath format of //*[@id="jobs_list-83877138"] where the number is a unique number
+  # In order to gind the next page arrow, and see if it is enabled or disabled
+  # nextLink <- remDr$findElement('xpath', "//a[@name=\"nextLink\"]")
+  # str_detect(nextLink$getElementAttribute("class")[[1]],"enabled")
+  # str_detect(nextLink$getElementAttribute("class")[[1]],"disabled")
   
-  # webElem <- remDr$findElement(using = 'id',value = "jobs_list")
-  # webElem.children <- webElem$findChildElements(using = 'class', "searchresult")
-  # webElem.children$value
+## Save the jobs from page 1, then loop through every page saving the jobs on that page
+  nextIsDisabled <- FALSE
+  pageID <- 0
+  write("Starting to collect jobs from Apple",file="log.txt",append = TRUE)
   
-  webElem <- remDr$findElement('xpath', value = "//*[@id=\"jobs_list\"]/tbody/tr[2]/td/p/a")
-  webElem$clickElement()
-  remDr$screenshot(display = TRUE) 
+  # In case there is only one page first we should get all the items on the first page
+  saveJobsFromPage()
   
-  jobDescElem <- remDr$findElement('xpath', "//*[@id=\"requisitionDescriptions\"]")
-    jobDescRaw <- jobDescElem$getElementAttribute("outerHTML")[[1]]
-    jobDescText <- htmlParse(jobDescRaw, asText = TRUE, encoding="UTF-8")
-      jobID <- capture.output(xpathApply(jobDescText, "//li")[[1]])
-      jobID <- str_replace_all(jobID,"<\\/?li>","")
-      jobID <- str_replace_all(jobID,":","")
-      jobID <- str_trim(jobID)
-      jobID <- str_replace_all(jobID," ","_")
-    setwd(paste0(getwd(),"/webscraping/Apple_job_scraping/files/"))
-    saveXML(jobDescText,
-            file = paste0(jobID,".xml"))
-      # Get back with htmlParse("Job_Number_113110538.xml")
+  while(nextIsDisabled == FALSE) {
+    nextLink <- remDr$findElement('xpath', "//a[@name=\"nextLink\"]")
+    if(str_detect(nextLink$getElementAttribute("class")[[1]],"enabled")==TRUE) {
+      nextLink$clickElement()
+      pageID <- pageID + 1
+      remDr$screenshot(display = TRUE)
+      # Wait a random amount of time before switching pages
+      Sys.sleep(round(runif(n=1,min=5,max=15),digits = 3))
+      
+      print(paste0("Getting values from page: ",pageID))
+      write(paste0("Getting values from page: ",pageID),file="log.txt",append = TRUE)
+      # After we've switched to a new page and delayed randomly it is time to get all the jobs on the page
+      saveJobsFromPage()
+      
+    } else {
+      nextIsDisabled = TRUE
+      print("No more pages detected")
+    }
     
-    
-    
+  }
+  
