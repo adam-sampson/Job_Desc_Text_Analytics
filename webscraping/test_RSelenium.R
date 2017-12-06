3981edad748de93df260d0cf1777118ff7d59c74
Nathan Thomas <nthomas2@bellarmine.edu>
2017-12-05 21:21:53 -0500

    Initial Commit

--- webscraping/test_RSelenium.R
+++
@@ -1,7 +0,0 @@
-library(RSelenium)
-remDr <- remoteDriver(remoteServerAddr = "192.168.99.100",
-                      +                       port = 4445L)
-remDr$open()
-
-remDr$navigate("http://www.google.com/ncr")
-remDr$getTitle()
