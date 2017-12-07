2ad088474e2960a1351d176bc4f3fb72809a3698
Adam <12676475+adam-sampson@users.noreply.github.com>
2017-12-07 12:13:16 -0500

    Add files via upload
    LDA script now can take Resumes after the model is trained and predict their topic.

--- Jobs_LDA.R
+++ Jobs_LDA.R
@@ -24,44 +24,63 @@ library(readr)
   resume.df <- str_replace_all(resume.df,"[\\r\\n\\t]"," ")
   resume.df <- data.frame(jobid = "SampsonResume", jobtitle = "none",jobdesc = resume.df,company = "resume")
   
-  jobs.df <- rbind(jobs.df,resume.df)
+  # jobs.df <- rbind(jobs.df,resume.df)
 
 #---
 # Clean data even more
 #---
-  # Remove everything except for [a-zA-Z] [:space:] ' + - and replace with space
-  jobs.df$jobdesc <- str_replace_all(jobs.df$jobdesc,"[^a-zA-Z[:space:]+\\-\']"," ")
-  # Remove ' and replace with nothing.
-  jobs.df$jobdesc <- str_replace_all(jobs.df$jobdesc,"\'","")
-  # Convert to utf-8 explicitly
-  jobs.df$jobdesc <- iconv(jobs.df$jobdesc, to = "utf-8")
+  cleanDataMore <- function(jobs.df) {
+    # Remove everything except for [a-zA-Z] [:space:] ' + - and replace with space
+    #jobs.df$jobdesc <- str_replace_all(jobs.df$jobdesc,"[^a-zA-Z[:space:]+\\-\']"," ")
+    # Remove everything except for [a-zA-Z] [:space:] ' + and replace with space
+    jobs.df$jobdesc <- str_replace_all(jobs.df$jobdesc,"[^a-zA-Z[:space:]+\']"," ")
+    # Remove ' and replace with nothing.
+    jobs.df$jobdesc <- str_replace_all(jobs.df$jobdesc,"\'","")
+    # Convert to utf-8 explicitly
+    jobs.df$jobdesc <- iconv(jobs.df$jobdesc, to = "utf-8")
+    return(jobs.df)
+  }
+  
+  jobs.df <- cleanDataMore(jobs.df = jobs.df)
+  resume.df <- cleanDataMore(jobs.df = resume.df)
+  
 #---
 # Prepare data for use as corpus and tdm
 #---
-  jobs.corpus <- Corpus(DataframeSource(data.frame(doc_id = jobs.df$jobid,
-                                                   text = jobs.df$jobdesc,
-                                                   stringsAsFactors = FALSE)))
-    # print(jobs.corpus)
-    # inspect(jobs.corpus[1:5])
-    # meta(jobs.corpus[[1]])
-    # inspect(jobs.corpus[[1]])
-    # lapply(jobs.corpus[[1]]$content[[2]],as.character)
-  
-  jobs.corpus <- tm_map(jobs.corpus,PlainTextDocument)
-  jobs.corpus <- tm_map(jobs.corpus,content_transformer(tolower))
-  jobs.corpus <- tm_map(jobs.corpus,removeWords,stopwords("english"))
-    # remove the names of the company (I.E. apple or apple's) from the text 
-  jobs.corpus <- tm_map(jobs.corpus,removeWords,tolower(c(unique(jobs.df$company),paste0(unique(jobs.df$company),"s"))))
-  jobs.corpus <- tm_map(jobs.corpus,removeWords,c("ã‚â"))
-  jobs.corpus <- tm_map(jobs.corpus,stripWhitespace)
-    # print(jobs.corpus)
-    # inspect(jobs.corpus[[1]])
-    # lapply(jobs.corpus[[1]]$content[[2]],as.character)
-  
-  jobs.dtm <- DocumentTermMatrix(jobs.corpus)
-    rownames(jobs.dtm) <- jobs.df$jobid
-    rownames(jobs.dtm)
+  convertToCleanCorpus <- function(jobs.df) {
+    jobs.corpus <- Corpus(DataframeSource(data.frame(doc_id = jobs.df$jobid,
+                                                     text = jobs.df$jobdesc,
+                                                     stringsAsFactors = FALSE)))
+      # print(jobs.corpus)
+      # inspect(jobs.corpus[1:5])
+      # meta(jobs.corpus[[1]])
+      # inspect(jobs.corpus[[1]])
+      # lapply(jobs.corpus[[1]]$content[[2]],as.character)
     
+    jobs.corpus <- tm_map(jobs.corpus,PlainTextDocument)
+    jobs.corpus <- tm_map(jobs.corpus,content_transformer(tolower))
+    jobs.corpus <- tm_map(jobs.corpus,removeWords,stopwords("english"))
+      # remove the names of the company (I.E. apple or apple's) from the text 
+    jobs.corpus <- tm_map(jobs.corpus,removeWords,tolower(c(unique(jobs.df$company),paste0(unique(jobs.df$company),"s"))))
+    jobs.corpus <- tm_map(jobs.corpus,removeWords,c("ã‚â"))
+    jobs.corpus <- tm_map(jobs.corpus,stripWhitespace)
+      # print(jobs.corpus)
+      # inspect(jobs.corpus[[1]])
+      # lapply(jobs.corpus[[1]]$content[[2]],as.character)
+    return(jobs.corpus)
+  }
+  
+  jobs.corpus <- convertToCleanCorpus(jobs.df)
+  
+  corpusToDTM <- function(jobs.corpus,jobs.df) {
+    jobs.dtm <- DocumentTermMatrix(jobs.corpus)
+      rownames(jobs.dtm) <- jobs.df$jobid
+      rownames(jobs.dtm)
+    return(jobs.dtm)
+  }
+  
+  jobs.dtm <- corpusToDTM(jobs.corpus,jobs.df)
+  
   freq <- colSums(as.matrix(jobs.dtm))
     length(freq)
     freq <- freq[order(freq,decreasing = TRUE)]
@@ -88,13 +107,22 @@ library(readr)
       head(as.data.frame(jobs.lda.topics))
       # View(as.data.frame(jobs.lda.topics))
         length(as.data.frame(jobs.lda.topics)[,1])
-      # Top 6 terms in each topic
-        jobs.lda.terms <- as.matrix(terms(jobs.lda,6))
-        head(as.data.frame(jobs.lda.terms))
+      # Top 10 terms in each topic
+        jobs.lda.terms <- as.matrix(terms(jobs.lda,k=10))
+        head(as.data.frame(jobs.lda.terms),n=10)
       # Probabilities associeated with each topic assignment
         topicProbabilities <- as.matrix(jobs.lda@gamma)
           rownames(topicProbabilities) <- jobs.df$jobid
-          View(as.data.frame(topicProbabilities,stringsAsFactors = FALSE))
+          head(as.data.frame(topicProbabilities,stringsAsFactors = FALSE),n=10)
+          # View(as.data.frame(topicProbabilities,stringsAsFactors = FALSE))
+          # Each of these values is an estimated proportion of words from that 
+          # document that are generated from that topic. For example, the model 
+          # estimates that only about ??% of the words in document 1 were 
+          # generated from topic 1.
+      # Probabilities associated with each term for each topic
+        termProbabilities <- as.matrix(jobs.lda@beta)
+          termProbabilities <- t(termProbabilities)
+          rownames(termProbabilities) <- jobs.lda@terms
       # Find relative importance of 2 topics
         topicProb <- function(x,t1,t2) {
           sort(topicProbabilities[x,])[k-(t1-1)]/sort(topicProbabilities[x,])[k-(t2-1)]
@@ -103,4 +131,19 @@ library(readr)
                                  topicProb,t1=1,t2=2)
         topic2ToTopic3 <- lapply(1:nrow(jobs.dtm),
                                  topicProb,t1=2,t2=3)
+ 
+#---
+# Comparing the LDA to new documents (resumes)
+# https://stackoverflow.com/questions/16115102/predicting-lda-topics-for-new-data
+#-
+  # Convert resume to dtm
+  resume.corpus <- convertToCleanCorpus(resume.df)
+  resume.dtm <- corpusToDTM(resume.corpus,resume.df)
+        
+  resume.topics <- topicmodels::posterior(jobs.lda,newdata = resume.dtm)
+    # Probabilities associeated with each topic assignment
+    topicProbabilities <- as.matrix(resume.topics$topics)
+      rownames(topicProbabilities) <- resume.df$jobid
+      colnames(topicProbabilities) <- paste("Topic",colnames(topicProbabilities))
+      head(as.data.frame(topicProbabilities,stringsAsFactors = FALSE),n=10)
       
