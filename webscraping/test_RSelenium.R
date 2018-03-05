f249ec2a167d7e7fee5c7542cc0931196010f52f
Adam <12676475+adam-sampson@users.noreply.github.com>
2018-03-04 20:20:11 -0500

    some google files

--- webscraping/test_RSelenium.R
+++ webscraping/test_RSelenium.R
@@ -1,6 +1,6 @@
 library(RSelenium)
 remDr <- remoteDriver(remoteServerAddr = "192.168.99.100",
-                      +                       port = 4445L)
+                      port = 4445L)
 remDr$open()
 
 remDr$navigate("http://www.google.com/ncr")
